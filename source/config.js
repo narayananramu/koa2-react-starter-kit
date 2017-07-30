161ed581dbaba5bd5f1e8adbf21df8f1a548621f
Narayanan M Ramanathan <nmr@Narayanans-MacBook-Pro.local>
2017-07-30 21:26:53 +0530

    configuration update

--- source/config.js
+++ source/config.js
@@ -1,10 +1,23 @@
+var redisStore = require('koa-redis');
 module.exports = {
     sessionConfiguration : {
-        key: 'koa:sess',
+        key: '_a',
         maxAge: 86400000,
         overwrite: true,
         httpOnly: true,
-        signed: true
+        signed: true,
+        domain: '',
+        store: redisStore({
+            host: "192.168.0.100",
+            port: 32769
+        })
     },
-    port: '3000'
+    port: '3000',
+    mongoConnection:{
+        host: '192.168.0.100',
+        port: '32768',
+        username: '',
+        password: '',
+        database: ''
+    }
 }
