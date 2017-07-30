8490f4dc22593801085efd7592135c5d71c7a574
Narayanan M Ramanathan <nmr@Narayanans-MacBook-Pro.local>
2017-07-30 21:29:01 +0530

    app configuration update

--- source/app.js
+++ source/app.js
@@ -6,24 +6,26 @@ import config from './config';
 import koa from 'koa';
 import staticServe from 'koa-static';
 import viewEngine from 'koa-ejs';
+import mongoose from 'mongoose';
 
 import session from 'koa-session';
 import router from './router';
 
 const app = new koa();
-
+mongoose.Promise = global.Promise;
+const connection = mongoose.connect("mongodb://"+config.mongoConnection.host+":"+config.mongoConnection.port+"/"+config.mongoConnection.database,{useMongoClient: true});
 app.keys = ['session_secret_key'];
 
-app.use(session(config.sessionConfiguration, app));
 app.use(staticServe(__dirname + '/public'));
+app.use(bodyParser());
+app.use(session(config.sessionConfiguration, app));
 app.use(router.routes());
-
 viewEngine(app, {
     root: path.join(__dirname, 'views'),
     layout: false,
     viewExt: 'ejs',
     cache: false,
-    debug: true
+    debug: false
 });
 
 app.listen(config.port);
